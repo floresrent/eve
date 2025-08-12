a9e864f091191a8f86e27f7343640d5d32be6e98
floresrent <gonzalezivonne354@gmail.com>
2025-08-11 22:28:05 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Component1 from './assets/components/components.jsx';
+
+function App(){
+    return(
+      <div>
+        <Component1
+          message="hello!"
+        />
+      </div>
+    );
+}
+
+export default App
---
+++ src/assets/components/components.jsx
@@ -0,0 +1,11 @@
+import Button from './button.jsx'
+
+function Component1({message}){
+  return(
+    <div>
+      <h1>{message}</h1>
+      <Button/>
+    </div>
+  );
+}
+export default Component1
